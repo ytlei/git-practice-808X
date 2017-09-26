7e90fecf360cd9abd831dba2f1146e0cd6950774
Yi-ting Lei <yitinglei0207@gmail.com>
2017-09-26 15:01:17 -0400

    add testing

---
+++ include/test.hpp
@@ -0,0 +1,24 @@
+/**
+ *  @File name: test.hpp
+ *
+ *  @brief: header file testing the pidController
+ *
+ *  @Created on: Sep 26, 2017
+ *  @Author: yitinglei
+ */
+
+#ifndef TEST_HPP_
+#define TEST_HPP_
+
+
+class testPIDController{
+
+public:
+	testPIDController();
+	~testPIDController();
+	void testSetPidGain();
+	void testCalResult();
+};
+
+
+#endif /* TEST_HPP_ */
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,37 @@
 #include <gtest/gtest.h>
-
+#include <iostream>
+#include <pidController.hpp>
+#include <test.hpp>
 TEST(dummy, should_pass) {
   EXPECT_EQ(1, 1);
+}
+
+
+/**
+ *   Test the setPidGain function
+ *
+ *
+ *   @param  none
+ *   @return none
+*/
+void testPIDController::testSetPidGain() {
+	pidController pid;
+	double x = 1.0;
+	pid.setPidGain(x,x,x);
+	assert(pid.getKp() == x);
+	assert(pid.getKi() == x);
+	assert(pid.getKd() == x);
+}
+
+/**
+ *   Test the calculate result velocity function
+ *
+ *
+ *   @param  none
+ *   @return none
+*/
+void testPIDController::testCalResult() {
+	pidController pid;
+	double x = 1.0;
+	assert(pid.calResult(x,x,x) == 0);
 }
